0074601b32fdbd52f1e0f21dfbb75f408e137513
Meg Galvez <meg@oakmaine.com>
2019-03-21 17:02:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,7 +71,26 @@ def current_player
   else turn_count.odd?
     return "O"
   end 
-
 end 
+
+def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      position_1 = @board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+      if ((position_1 == "X" && position_2 == "X" && position_3 == "X") ||     (position_1 == "O" && position_2 == "O" && position_3 == "O"))
+         return win_combination
+      else 
+    end 
+  end 
+    return nil 
+ end
+
+
+
+
 
 end 
