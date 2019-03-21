2ce8eacb88cccef0e6e2958fafc61fab2b9f923a
Meg Galvez <meg@oakmaine.com>
2019-03-21 17:00:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,4 +39,16 @@ def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
 end
 
+def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(index)
+    move(index, current_player)
+    display_board
+  else
+    turn
+  end
+end
+
 end 
